6faf974d8bb9c40bbae60d29241f3634a7ee3de5
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-07-09 23:48:23 +0900

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def goodbye
-    render html: "goodbye,world!"
+  def hello
+    render html: "hello, world!"
   end
 end
